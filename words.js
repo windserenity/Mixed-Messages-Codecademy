ec85f2ec7579a3e5400103c595fabd45200b004f
windserenity <95578077+windserenity@users.noreply.github.com>
2022-01-15 14:39:18 -0800

    one sylb words array complate

--- words.js
+++ words.js
@@ -1,3 +1,15 @@
+const oneSylbWord = [
+    'plop','rout','bleach','soak','throes','class','dive','drown','crouch','yawn','wrench','tale',
+    'cry','law','kid','lob','boat','drawl','reuse','lord','date','girth','crisp','crook','dog','crow','ice','tarp',
+    'die','dung','mean','script','kit','sniff','spit','wear','fine','mine','star','taint','masked','parked','gloved',
+    'slump','hill','streak','there','nail','wide','clamp','do','flunk','right','snug','leash','scour','just','ace',
+    'smite','lurch','seal','cat','snag','thong','chat','stew','free','mock','snow','bend','ground','damn','beep',
+    'stroke','stance','lot','jig','puck','rent','dumb','bat','trained','sail','raise','wall','learn','porn','plug',
+    'heave','off','clock','lack','bliss','prized','path','tank','crest','cooked','serve','slim',
+];
+const twoSylbWord = [];
+const threeSylbWord = [];
+const fourSylbWord = [];
 const fiveSylbWord = [
     'disobedience','promiscuity','patriotism','mythological','ineligible','sociology','panamanian',
     'fortification','metaphorical','popularity','psychotherapy','irresistible','anorexia','victimization',
@@ -15,6 +27,7 @@ const fiveSylbWord = [
     'inefficiency','observational','vice-president','denomination','theoretical','euro-american','palestinian',
     'constitutional',
 ];
+const sixSylbWord = [];
 const sevenSylbWord = [    
     'multimillion-dollar','environmentalism','inevitability','interdisciplinary','individuality',
     'computer-generated','african-american','anesthesiologist','unpredictability','multidimensional',
@@ -24,4 +37,6 @@ const sevenSylbWord = [
     'epidemiology','autobiographical','secretary-general','telecommunications','constitutionality','sociopolitical',
     'african-american',
 ];
+
+
 
