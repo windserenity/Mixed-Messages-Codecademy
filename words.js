b4f2d37c6f68831e8e0810dd5a4df8e7d1aba4b0
windserenity <95578077+windserenity@users.noreply.github.com>
2022-01-15 14:19:20 -0800

    formated words added to 5 sylebyl array

---
+++ words.js
@@ -0,0 +1,18 @@
+const fiveSylbWord = [
+    'disobedience','promiscuity','patriotism','mythological','ineligible','sociology','panamanian',
+    'fortification','metaphorical','popularity','psychotherapy','irresistible','anorexia','victimization',
+    'criminality','exploratory','inspirational','aboriginal','inactivity','congregational','standardization',
+    'socialization','mediterranean','capitalism','theological','inability','camaraderie','administration',
+    'disingenuous','deregulation','postsecondary','colonization','undemocratic','obligatory','interminable',
+    'authorization','innumerable','aficionado','exhilaration','polarization','evangelical','preservationist',
+    'multiplicity','justifiable','environmental','possibility','archaeology','infidelity','cafeteria',
+    'entomologist','mediocrity','refrigerated','hypocritical', 'unfathomable','computational','disability',
+    'temperamental','opportunistic','contemporary','imaginary','vegetarian','unavoidable','disintegration',
+    'inadequacy','appalachian','determinism','generational','articulated','instructional','memorialize',
+    'unpredictable','third-generation','senatorial','visibility','self-sufficiency','secularism','retaliation',
+    'abomination','territorial','exaggerated','ambiguity','million-dollar','self-expression','repository',
+    'radicalism','understandable','appreciative','materialize','manufacturer','impenetrable','inconceivable',
+    'inefficiency','observational','vice-president','denomination','theoretical','euro-american','palestinian','constitutional',
+];
+const sevenSylbWord = [];
+
