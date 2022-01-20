c73e720ee5d164bbceb769be9f03e8782c076391
windserenity <95578077+windserenity@users.noreply.github.com>
2022-01-19 20:31:49 -0800

    refactor random workds into function

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 import {oneSylbWord as one, twoSylbWord as two, threeSylbWord as three, fourSylbWord as four, fiveSylbWord as five, sixSylbWord as six, sevenSylbWord as seven } from './words.js';
 
 
-const randomWordSelector = (wordArray) => { //function to slecet a random word from any given array
+const randomWordSelector = wordArray => { //function to slecet a random word from any given array
     let indexOfArray = Math.floor(Math.random() * wordArray.length +1);
     return wordArray[indexOfArray];
 };
 
 
-function randommArraySelector() { //randomly selected array with the index retunred as object for hiku syllables
+const randommArraySelector = () => { //randomly selected array with the index retunred as object for hiku syllables
     let selector = [one, two, three, four, five, six, seven];
         let index =  Math.floor(Math.random() * selector.length); 
              let obj = {
@@ -18,19 +18,17 @@ function randommArraySelector() { //randomly selected array with the index retun
     return obj;  
 };
 
-/* let word = randommArraySelector();
-console.log(randomWordSelector(word.words));
-console.log(word.syllables+1);
- */
 
-let syllables = 0; 
-    while (syllables<7 ) {
-        
+    const wordsWithSylbalCount = sylbNum => {
+
         let word = randommArraySelector();
         console.log(randomWordSelector(word.words));
         console.log(word.syllables+1); 
+    
         
-        syllables ++
+        
     };
+   
 
 
+ console.log();
